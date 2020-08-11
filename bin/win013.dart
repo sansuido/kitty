75433e3d985b3c6bc51f8cc88796e5c84c2545bd
oda <oda@ad.ibic.co.jp>
2020-08-11 13:40:08 +0900

    update

---
+++ bin/win013.dart
@@ -0,0 +1,54 @@
+// http://wisdom.sakura.ne.jp/system/winapi/win32/win13.html
+import 'dart:ffi';
+import 'package:kitty/win32.dart';
+
+final hInstance = GetModuleHandle(nullptr);
+
+int windowProc(int hWnd, int uMsg, int wParam, int lParam) {
+  switch(uMsg) {
+    case WM_CREATE:
+      final cs = Pointer<CREATESTRUCT>.fromAddress(lParam).ref;
+      final nYesNo = MessageBox(hWnd, TEXT('ウィンドウを生成しますか?'), cs.lpszName, MB_YESNO | MB_ICONQUESTION);
+      if (nYesNo == IDYES) return 0;
+      return -1;
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      return 0;
+  }
+  return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int main(List arguments) {
+  final wc = WNDCLASS.allocate();
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.lpfnWndProc = Pointer.fromFunction<WindowProc>(windowProc, 0);
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hInstance = hInstance;
+  wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wc.hbrBackground = GetStockObject(WHITE_BRUSH);
+  wc.lpszMenuName = nullptr;
+  wc.lpszClassName = TEXT('KITTY');
+  if (RegisterClass(wc.addressOf) == 0) return 0;
+  final hWnd = CreateWindowEx(
+      0,
+      TEXT('KITTY'),
+      TEXT('Kitty on your lap'),
+      WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+      100,
+      100,
+      200,
+      200,
+      NULL,
+      NULL,
+      hInstance,
+      nullptr);
+  if (hWnd == 0) return 0;
+  final msg = MSG.allocate();
+  while (GetMessage(msg.addressOf, NULL, 0, 0) != 0) {
+    TranslateMessage(msg.addressOf);
+    DispatchMessage(msg.addressOf);
+  }
+  return 0;
+}
