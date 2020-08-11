2fc4b7bd6f23b3dc5d24f6b46fe9b8b0a2fa52bd
oda <oda@ad.ibic.co.jp>
2020-08-11 14:08:57 +0900

    update

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,5 @@
-import 'win006.dart' as win006;
-import 'win007.dart' as win007;
-import 'win008.dart' as win008;
-import 'win009.dart' as win009;
-import 'win010.dart' as win010;
+import 'win014.dart' as win014;
 
 void main(List arguments) {
-  win006.main(arguments);
-  win007.main(arguments);
-  win008.main(arguments);
-  win009.main(arguments);
-  win010.main(arguments);
+  win014.main(arguments);
 }
