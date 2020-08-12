690b75f1205c4f78c5ff8d3522396c574840ba80
oda <oda@ad.ibic.co.jp>
2020-08-12 10:37:23 +0900

    update

---
+++ bin/win021.dart
@@ -0,0 +1,61 @@
+// http://wisdom.sakura.ne.jp/system/winapi/win32/win21.html
+import 'dart:ffi';
+import 'package:kitty/win32.dart';
+
+final hInstance = GetModuleHandle(nullptr);
+
+int windowProc(int hWnd, int uMsg, int wParam, int lParam) {
+  switch (uMsg) {
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      return 0;
+    case WM_PAINT:
+      final lptStr = TEXT('Kitty on your lap');
+      final tm = TEXTMETRIC.allocate();
+      final ps = PAINTSTRUCT.allocate();
+      final hdc = BeginPaint(hWnd, ps.addressOf);
+      GetTextMetrics(hdc, tm.addressOf);
+      SetBkColor(hdc, RGB(0xff, 0, 0));
+      TextOut(hdc, 10, 10, lptStr, lstrlen(lptStr));
+      SetBkMode(hdc, TRANSPARENT);
+      TextOut(hdc, 10, 10 + tm.tmHeight, lptStr, lstrlen(lptStr));
+      EndPaint(hWnd, ps.addressOf);
+      return 0;
+  }
+  return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int main(List arguments) {
+  final wc = WNDCLASS.allocate();
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.lpfnWndProc = Pointer.fromFunction<WindowProc>(windowProc, 0);
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hInstance = hInstance;
+  wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+  wc.hbrBackground = GetStockObject(WHITE_BRUSH);
+  wc.lpszMenuName = nullptr;
+  wc.lpszClassName = TEXT('KITTY');
+  if (RegisterClass(wc.addressOf) == 0) return 0;
+  final hWnd = CreateWindowEx(
+      0,
+      TEXT('KITTY'),
+      TEXT('Kitty on your lap'),
+      WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      NULL,
+      NULL,
+      hInstance,
+      nullptr);
+  if (hWnd == 0) return 0;
+  final msg = MSG.allocate();
+  while (GetMessage(msg.addressOf, NULL, 0, 0) != 0) {
+    TranslateMessage(msg.addressOf);
+    DispatchMessage(msg.addressOf);
+  }
+  return msg.wParam;
+}
